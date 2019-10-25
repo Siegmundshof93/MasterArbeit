b74af0d82634702b2bd619b3efcf04e5abd5a882
pvl <pav77788@gmail.com>
2019-10-25 14:38:37 +0200

    720

--- PowerBudget.m
+++ PowerBudget.m
@@ -4,51 +4,38 @@ clc
 h = 600e3;
 mu = 398600e9;
 Re = 6378e3; #radius of Earth
-grad = 0:360-1;
-stromChar = 0.5;
-stromDisch = 0.5;
+grad = 0:1080-1;
+stromChar = 2;
+stromDisch = 1;
 Rleo = Re + h; #radius of LEO orbit
 V = 7.4;
 eclipse = 2*asind(Re/Rleo); #eclipse angle
 T = 2 * pi * sqrt(Rleo**3/mu);# time of the period 
 dt = T/360/3600; # time pro 1 grad
-#darkT = eclipse * dt; # time in eclipse
-#lightT = (360 - eclipse) * dt; # time in light
-day = 360 - eclipse;
+day = round(360 - eclipse);
+
+
 Wh_vec = zeros(1,length(grad));
-Wh(1) = V * (stromChar * 1.3) * dt;
-dWh(1) = (V * (stromChar * 1.3)* 2 * dt) - Wh(1);
+
 
 
 
 for i = 1:length(grad)-1 
   
-#  Wh(i+1) =  V * (stromChar * 1.3)* (i+1) * dt;
-#  dWh(i+1) = (V * (stromChar * 1.3)* (i+2) * dt) - Wh(i+1); 
-  if i <= round(day)
+  if i <= day
     Wh_vec(i)=V * (stromChar * 1.3)* (i+1) * dt;
+  elseif i > day & i < 360
+    Wh_vec(i)=Wh_vec(day) - V * (stromChar * 1.3)* (i-day) * dt;
+  elseif i >= 360 & i <= 360 + day
+    Wh_vec(i)=V * (stromChar * 1.3)* (i-261) * dt;
+  elseif i > 360 + day & i < 720
+    Wh_vec(i)=Wh_vec(day + 360) - V * (stromChar * 1.3)* (i-(360 + day)) * dt;
+  elseif i >= 720 & i <= 720 + day 
+     Wh_vec(i)=V * (stromChar * 1.3)* (i-522) * dt;
   else
-    Wh_vec(i)=Wh_vec(round(day)) - V * (stromChar * 1.3)* (i-round(day)) * dt;
+    Wh_vec(i)=Wh_vec(day+ 720) - V * (stromChar * 1.3)* (i-(720 + day)) * dt;
   end
-  
-% if i < day 
-%  
-% 
-%  Wh(i+1) = Wh(i+1) + dWh(i+1);
-% 
-%#elseif i > 360 & i < 360 + day
-% #   Wh(i+1) =  V * (stromChar * 1.3)* i * dt;
-%#  dWh(i+1) = (V * (stromChar * 1.3)* i+1 * dt) - Wh(i+1);
-%
-% #elseif i > 720 & i < 720 + day 
-%#   Wh(i+1) =  V * (stromChar * 1.3)* i * dt;
-% # dWh(i+1) = (V * (stromChar * 1.3)* i+1 * dt) - Wh(i+1);
-% 
-%else
-%  
-%   Wh(i+1) = Wh(i+1) - dWh(i+1);
 
-%end
 end
 
 
